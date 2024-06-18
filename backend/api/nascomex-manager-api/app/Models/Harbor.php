04de00bdb5a7a90ca7bec8ffef1cb0b98c903d2b
ElMarcelFarias <marcel.leitefarias@gmail.com>
2024-06-17 23:27:44 -0300

    New: Finalizando crud de portos!

--- backend/api/nascomex-manager-api/app/Models/Harbor.php
+++ backend/api/nascomex-manager-api/app/Models/Harbor.php
@@ -8,4 +8,19 @@ use Illuminate\Database\Eloquent\Model;
 class Harbor extends Model
 {
     use HasFactory;
+
+    protected $fillable = ['name', 'state'];
+
+    public static function createHarbor(array $data): self
+    {
+        return self::create($data);
+    }
+
+    public static function updateHarbor(int $id, array $data): self
+    {
+        $harbor = self::findOrFail($id);
+        $harbor->update($data);
+
+        return $harbor;
+    }
 }
